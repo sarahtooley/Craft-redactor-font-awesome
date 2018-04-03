85c664f17511063bbf86e2927072065926e57d0d
sarahtooley <sarah.tooley@medavie.bluecross.ca>
2018-04-03 15:57:20 -0300

    Added JSON files and fixed styles.

--- coredactorfontawesome/resources/fontAwesome.js
+++ coredactorfontawesome/resources/fontAwesome.js
@@ -5,14 +5,16 @@ RedactorPlugins.creativeorangeFontAwesome = function () {
         init: function () {
             var button = this.button.add('creativeorangeFontAwesome', 'Font Awesome icons');
             this.button.addCallback(button, this.creativeorangeFontAwesome.show);
-            this.button.setIcon(button, '<i class="fa fa-smile-o"</i>');
+            this.button.setIcon(button, '<i class="far fa-smile"</i>');
         },
         getTemplate: function (modal, len, icons) {
             return String()
+                + '<script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>\n'
                 + '<section id="redactor-modal-advanced">'
-                + '<div class="iconFilterStyle"><p>Choose a theme and style or search for an icon:</p>'
+                + '<div class="iconFilterStyle"><p>Choose a category and style or search for individual icons:</p>'
                 + '<select name="iconTheme" id="iconStyle">'
-                + '<option value="all">All (2,707 icons)</option>'
+                + '<option value="" disabled="true" selected>Choose a category...</option>'
+                + '<option value="all">All Themes</option>'
                 + '<option value="accessibility">Accessibility</option>'
                 + '<option value="arrows">Arrows</option>'
                 + '<option value="audiovideo">Audio & Video</option>'
@@ -46,9 +48,10 @@ RedactorPlugins.creativeorangeFontAwesome = function () {
                 + '<option value="people">Users & People</option>'
                 + '<option value="vehicles">Vehicles</option>'
                 + '<option value="writing">Writing</option>'
-                + '</select>'
+                + '</select><br><br>'
                 + '<select name="iconTheme" id="iconStyle">'
-                + '<option value="all">All</option>'
+                + '<option value="" disabled="true" selected>Choose a style...</option>'
+                + '<option value="all">All Styles</option>'
                 + '<option value="fas">Solid</option>'
                 + '<option value="far">Regular</option>'
                 + '<option value="fal">Light</option>'
@@ -65,63 +68,81 @@ RedactorPlugins.creativeorangeFontAwesome = function () {
                 + '</section>';
         },
         show: function () {
-            // Load and select the icons.
-            var accessibility = ['fab fa-accessible-icon', 'fas fa-american-sign-language-interpreting', 'fas fa-assistive-listening-systems', 'fas fa-audio-description', 'fas fa-blind', 'fas fa-braille', 'fas fa-closed-captioning', 'far fa-closed-captioning', 'fas fa-deaf', 'fas fa-low-vision', 'fas fa-phone-volume', 'fas fa-question-circle', 'far fa-question-circle', 'fas fa-sign-language', 'fas fa-tty', 'fas fa-universal-access', 'fas fa-wheelchair',];
-            var arrows = [];
-            var audiovideo = [];
-            var business = [];
-            var charity = [];
-            var chat = [];
-            var chess = [];
-            var code = [];
-            var communication = [];
-            var computers = [];
-            var currency = [];
-            var datetime = [];
-            var design = [];
-            var editors = [];
-            var files = [];
-            var genders = [];
-            var hands = [];
-            var health = [];
-            var images = [];
-            var interfaces = [];
-            var logistics = [];
-            var maps = [];
-            var medical = [];
-            var moving = [];
-            var iobjects = [];
-            var payments = [];
-            var shapes = [];
-            var spinners = [];
-            var sports = [];
-            var istatus = [];
-            var people = [];
-            var vehicles = [];
-            var writing = [];
-            var all = accessibility.concat(arrows, audiovideo, business, charity, chat, chess, code, communication, computers, currency, datetime, design, editors, files, genders, hands, health, images, interfaces, logistics, maps, medical, moving, iobjects, payments, shapes, spinners, sports, istatus, people, vehicles, writing)
+            // Load all icons and categories.
+            var icons = null;
+            var categories = null;
 
-            // var icons = ['rub', 'ruble', 'rouble', 'pagelines', 'stack-exchange', 'arrow-circle-o-right', 'arrow-circle-o-left', 'caret-square-o-left', 'toggle-left', 'dot-circle-o', 'wheelchair', 'vimeo-square', 'try', 'turkish-lira', 'plus-square-o', 'adjust', 'anchor', 'archive', 'arrows', 'arrows-h', 'arrows-v', 'asterisk', 'ban', 'bar-chart-o', 'barcode', 'bars', 'beer', 'bell', 'bell-o', 'bolt', 'book', 'bookmark', 'bookmark-o', 'briefcase', 'bug', 'building-o', 'bullhorn', 'bullseye', 'calendar', 'calendar-o', 'camera', 'camera-retro', 'caret-square-o-down', 'caret-square-o-left', 'caret-square-o-right', 'caret-square-o-up', 'certificate', 'check', 'check-circle', 'check-circle-o', 'check-square', 'check-square-o', 'circle', 'circle-o', 'clock-o', 'cloud', 'cloud-download', 'cloud-upload', 'code', 'code-fork', 'coffee', 'cog', 'cogs', 'comment', 'comment-o', 'comments', 'comments-o', 'compass', 'credit-card', 'crop', 'crosshairs', 'cutlery', 'dashboard', 'desktop', 'dot-circle-o', 'download', 'edit', 'ellipsis-h', 'ellipsis-v', 'envelope', 'envelope-o', 'eraser', 'exchange', 'exclamation', 'exclamation-circle', 'exclamation-triangle', 'external-link', 'external-link-square', 'eye', 'eye-slash', 'female', 'fighter-jet', 'film', 'filter', 'fire', 'fire-extinguisher', 'flag', 'flag-checkered', 'flag-o', 'flash', 'flask', 'folder', 'folder-o', 'folder-open', 'folder-open-o', 'frown-o', 'gamepad', 'gavel', 'gear', 'gears', 'gift', 'glass', 'globe', 'group', 'hdd-o', 'headphones', 'heart', 'heart-o', 'home', 'inbox', 'info', 'info-circle', 'key', 'keyboard-o', 'laptop', 'leaf', 'legal', 'lemon-o', 'level-down', 'level-up', 'lightbulb-o', 'location-arrow', 'lock', 'magic', 'magnet', 'mail-forward', 'mail-reply', 'mail-reply-all', 'male', 'map-marker', 'meh-o', 'microphone', 'microphone-slash', 'minus', 'minus-circle', 'minus-square', 'minus-square-o', 'mobile', 'mobile-phone', 'money', 'moon-o', 'music', 'pencil', 'pencil-square', 'pencil-square-o', 'phone', 'phone-square', 'picture-o', 'plane', 'plus', 'plus-circle', 'plus-square', 'plus-square-o', 'power-off', 'print', 'puzzle-piece', 'qrcode', 'question', 'question-circle', 'quote-left', 'quote-right', 'random', 'refresh', 'reply', 'reply-all', 'retweet', 'road', 'rocket', 'rss', 'rss-square', 'search', 'search-minus', 'search-plus', 'share', 'share-square', 'share-square-o', 'shield', 'shopping-cart', 'sign-in', 'sign-out', 'signal', 'sitemap', 'smile-o', 'sort', 'sort-alpha-asc', 'sort-alpha-desc', 'sort-amount-asc', 'sort-amount-desc', 'sort-asc', 'sort-desc', 'sort-down', 'sort-numeric-asc', 'sort-numeric-desc', 'sort-up', 'spinner', 'square', 'square-o', 'star', 'star-half', 'star-half-empty', 'star-half-full', 'star-half-o', 'star-o', 'subscript', 'suitcase', 'sun-o', 'superscript', 'tablet', 'tachometer', 'tag', 'tags', 'tasks', 'terminal', 'thumb-tack', 'thumbs-down', 'thumbs-o-down', 'thumbs-o-up', 'thumbs-up', 'ticket', 'times', 'times-circle', 'times-circle-o', 'tint', 'toggle-down', 'toggle-left', 'toggle-right', 'toggle-up', 'trash-o', 'trophy', 'truck', 'umbrella', 'unlock', 'unlock-alt', 'unsorted', 'upload', 'user', 'users', 'video-camera', 'volume-down', 'volume-off', 'volume-up', 'warning', 'wheelchair', 'wrench', 'check-square', 'check-square-o', 'circle', 'circle-o', 'dot-circle-o', 'minus-square', 'minus-square-o', 'plus-square', 'plus-square-o', 'square', 'square-o', 'bitcoin', 'btc', 'cny', 'dollar', 'eur', 'euro', 'gbp', 'inr', 'jpy', 'krw', 'money', 'rmb', 'rouble', 'rub', 'ruble', 'rupee', 'try', 'turkish-lira', 'usd', 'won', 'yen', 'align-center', 'align-justify', 'align-left', 'align-right', 'bold', 'chain', 'chain-broken', 'clipboard', 'columns', 'copy', 'cut', 'dedent', 'eraser', 'file', 'file-o', 'file-text', 'file-text-o', 'files-o', 'floppy-o', 'font', 'indent', 'italic', 'link', 'list', 'list-alt', 'list-ol', 'list-ul', 'outdent', 'paperclip', 'paste', 'repeat', 'rotate-left', 'rotate-right', 'save', 'scissors', 'strikethrough', 'table', 'text-height', 'text-width', 'th', 'th-large', 'th-list', 'underline', 'undo', 'unlink', 'angle-double-down', 'angle-double-left', 'angle-double-right', 'angle-double-up', 'angle-down', 'angle-left', 'angle-right', 'angle-up', 'arrow-circle-down', 'arrow-circle-left', 'arrow-circle-o-down', 'arrow-circle-o-left', 'arrow-circle-o-right', 'arrow-circle-o-up', 'arrow-circle-right', 'arrow-circle-up', 'arrow-down', 'arrow-left', 'arrow-right', 'arrow-up', 'arrows', 'arrows-alt', 'arrows-h', 'arrows-v', 'caret-down', 'caret-left', 'caret-right', 'caret-square-o-down', 'caret-square-o-left', 'caret-square-o-right', 'caret-square-o-up', 'caret-up', 'chevron-circle-down', 'chevron-circle-left', 'chevron-circle-right', 'chevron-circle-up', 'chevron-down', 'chevron-left', 'chevron-right', 'chevron-up', 'hand-o-down', 'hand-o-left', 'hand-o-right', 'hand-o-up', 'long-arrow-down', 'long-arrow-left', 'long-arrow-right', 'long-arrow-up', 'toggle-down', 'toggle-left', 'toggle-right', 'toggle-up', 'arrows-alt', 'backward', 'compress', 'eject', 'expand', 'fast-backward', 'fast-forward', 'forward', 'pause', 'play', 'play-circle', 'play-circle-o', 'step-backward', 'step-forward', 'stop', 'youtube-play', 'adn', 'android', 'apple', 'bitbucket', 'bitbucket-square', 'bitcoin', 'btc', 'css3', 'dribbble', 'dropbox', 'facebook', 'facebook-square', 'flickr', 'foursquare', 'github', 'github-alt', 'github-square', 'gittip', 'google-plus', 'google-plus-square', 'html5', 'instagram', 'linkedin', 'linkedin-square', 'linux', 'maxcdn', 'pagelines', 'pinterest', 'pinterest-square', 'renren', 'skype', 'stack-exchange', 'stack-overflow', 'trello', 'tumblr', 'tumblr-square', 'twitter', 'twitter-square', 'vimeo-square', 'vk', 'weibo', 'windows', 'xing', 'xing-square', 'youtube', 'youtube-play', 'youtube-square', 'ambulance', 'h-square', 'hospital-o', 'medkit', 'plus-square', 'stethoscope', 'user-md', 'wheelchair'];
-            var icons = all;
+            var iconsXML = new XMLHttpRequest();
+            var categoriesXML = new XMLHttpRequest();
 
-            var len = icons.length;
 
-            this.modal.addTemplate('creativeorangeFontAwesome', this.creativeorangeFontAwesome.getTemplate(len, icons));
+
+            categoriesXML.onload = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    categories = JSON.parse(this.responseText);
+                }
+            };
+            categoriesXML.onerror = function(e) {
+                console.log(this.statusText);
+            };
+
+            iconsXML.open("GET", "../../../assets/json/faicons.json", true);
+            iconsXML.onload = function(e) {
+                if (this.readyState == 4 && this.status == 200) {
+                    icons = JSON.parse(this.responseText);
+                }
+            };
+            iconsXML.onerror = function(e) {
+                console.log(this.statusText);
+            };
+            iconsXML.send();
+
+
+            categoriesXML.open("GET", "../../../assets/json/facategories.json", true);
+            categoriesXML.send();
+
+            this.modal.addTemplate('creativeorangeFontAwesome', this.creativeorangeFontAwesome.getTemplate(icons.length, icons));
             this.modal.load('creativeorangeFontAwesome', 'Font awesome icons', 800);
-            //this.modal.createCancelButton();
-
-
-            var lencount = icons.length;
+            // this.modal.createCancelButton();
 
             var blkstr = [];
-            for (var z = 0; z < lencount; z++) {
-                var icon = icons[z];
-                //console.log(icon);
 
+            icons.forEach(function(icon) {
+                var iconCategory = '';
+                var iconStyle = '';
+                console.log(icon);
+
+                //Get the icon category.
+                for (var i = 0; i < categories; i++) {
+                    var category = categories[i];
+                    for (var i = 0; i < category[0]; i++) {
+                        var compareIcon = category[0][i];
+                        if (icon === compareIcon) {
+                            iconCategory = categories[i];
+                        }
+                    }
+                }
+
+                //Get the icon styles.
+                for (var i = 0; i < icon.styles; i++) {
+                    var style = icon.styles[i]
+                    if (style === "brand") {
+                        iconStyle = 'fab';
+                    } else if (style === "solid") {
+                        iconStyle = 'fas';
+                    } else if (style === "regular") {
+                        iconStyle = 'far';
+                    } else if (style === "light") {
+                        iconStyle = 'fal';
+                    }
+                }
                 // var listcons = $('<i class="' + icon + '" id="redactor-fa-' + icon + '"><span>' + icon + '</span></i>');
-                blkstr.push('<div><i class="' + icons[z] + '" id="redactor-fa-' + icons[z] + '"></i><br><span>' + icons[z] + '</span></div>');
+                blkstr.push('<div><i class="' + iconCategory + iconStyle + ' fa-' + icon + '" id="redactor-fa-' + icon + '"></i><br><span>' + icon.label + '</span></div>');
 
-            }
+            });
+            // for (var i = 0; i < icons.length; i++) {
+            //
+            // }
 
             $(".iconContents").html(blkstr.join(""));
 
